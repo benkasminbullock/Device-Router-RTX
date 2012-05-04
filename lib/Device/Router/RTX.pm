2512de1bd5f6f1184d9e6b3e840260730606a707
Ben Bullock <benkasminbullock@gmail.com>
2012-05-04 14:57:28 +0900

    Initial commit of Device-Router-RTX

---
+++ lib/Device/Router/RTX.pm
@@ -0,0 +1,13 @@
+=head1 NAME
+
+Device::Router::RTX
+
+=cut
+package Device::Router::RTX;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
